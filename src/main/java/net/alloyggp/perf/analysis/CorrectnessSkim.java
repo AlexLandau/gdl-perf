de2253248c4b536fc947bc4a38457a5b0a85128c
AlexLandau <alandau@cs.stanford.edu>
2016-08-18 21:49:02 -0700

    Output tweaks for correctness skim
    (cherry picked from commit 3436faee99ea4a8d5b42ade9aab31d3718a8ee71)

--- src/main/java/net/alloyggp/perf/analysis/CorrectnessSkim.java
+++ src/main/java/net/alloyggp/perf/analysis/CorrectnessSkim.java
@@ -2,8 +2,11 @@ package net.alloyggp.perf.analysis;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Set;
+import java.util.SortedSet;
 
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.google.common.collect.Sets;
@@ -21,7 +24,8 @@ public class CorrectnessSkim {
         resultsMap = Multimaps.filterValues(resultsMap, result -> gamesToConsider.contains(result.getGameKey()));
 
         System.out.println("Number of games considered: " + gamesToConsider.size());
-        for (EngineVersion ev : resultsMap.keySet()) {
+        SortedSet<EngineVersion> engineVersions = ImmutableSortedSet.copyOf(Comparator.comparing(EngineVersion::toString), resultsMap.keySet());
+        for (EngineVersion ev : engineVersions) {
             System.out.println("For engine " + ev.toString() + ":");
             Multimap<GameKey, CorrectnessTestResult> resultsByGame = CorrectnessTestResult.groupByGame(resultsMap.get(ev));
             int successfulSoFarCount = 0;
@@ -44,7 +48,12 @@ public class CorrectnessSkim {
             System.out.println("  Successful so far: " + successfulSoFarCount);
             System.out.println("  Untested: " + untestedCount);
             System.out.println("  Errors: " + errorsFoundCount);
+            System.out.println("    Games with errors: " + sort(gamesWithErrors));
         }
+    }
+
+    private static String sort(Set<GameKey> gamesWithErrors) {
+        return ImmutableSortedSet.copyOf(Comparator.comparing(GameKey::toString), gamesWithErrors).toString();
     }
 
 }
